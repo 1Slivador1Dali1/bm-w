c3041a49417f7d0d8ae5519f4cc4dd681a09f0a2
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-12-21 00:19:37 +0300

    bl2.st

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,87 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className=" min-h-screen bg-[var(--cvet-teksta-2)]  pt-[120px]">
+      <div className="max-w-[1612px] flex flex-col mx-auto justify-center gap-7">
+        <div className="flex flex-col gap-7 w-full">
+          <h2 className="text-[var(--cvet-teksta)] font-bold uppercase text-[55px] leading-[4.5rem] text-justify">
+            Мы создаем
+            <br /> цифровую реальность
+          </h2>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <p className="text-[var(--cvet-teksta)] text-wrap font-normal text-[24px] leading-[2rem]">
+            Мы — команда, которая специализируется на создании сайтов под ключ:
+            от профессионального веб-дизайна
+            <br /> до разработки сайтов для бизнеса любой сложности. Мы
+            предлагаем:
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="flex items-start gap-24 max-w-[1612px] pl-[30px]">
+          <Image src="/logo.png" alt="Example" width={426} height={504} />
+
+          <div className="max-w-[910px] flex flex-col gap-[34px]">
+            <div className="max-w-[910px] h-[434px] relative">
+              <div className="bg-[var(--osnovnoy-cvet-2)] rounded-[50px] w-[910px] h-[162px] absolute left-0 top-0 overflow-hidden">
+                <div className="flex flex-col gap-3 items-start justify-start max-w-[587px] absolute left-[92px] top-8">
+                  <div className="text-[var(--cvet-teksta-2)] text-left  text-[24px] font-bold relative w-[340px] flex items-center justify-start">
+                    Уникальные сайты с нуля
+                  </div>
+                  <div className="text-[var(--cvet-teksta-2)] text-left text-[24px] font-normal relative w-[587px] flex items-center justify-start">
+                    идеально адаптированные под ваш бизнес
+                  </div>
+                </div>
+                <Image
+                  src="/portal28.svg"
+                  alt=""
+                  className="w-7 h-7 absolute left-11 top-[34px] overflow-visible"
+                  width={28}
+                  height={28}
+                />
+              </div>
+              <div className="bg-[var(--osnovnoy-cvet)] rounded-[50px] w-[910px] h-[162px] absolute left-0 top-[136px] overflow-hidden">
+                <div className="flex flex-col gap-3 items-start justify-start max-w-[649px] absolute left-[92px] top-8">
+                  <div className="text-[var(--cvet-teksta)] text-left text-2xl font-normal relative w-[339px] flex items-center justify-start">
+                    Индивидуальный подход
+                  </div>
+                  <div className="text-[var(--cvet-teksta)] text-left text-2xl font-normal relative w-[649px] flex items-center justify-start">
+                    качественное сопровождение на каждом этапе
+                  </div>
+                </div>
+                <Image
+                  src="/portal28black.svg"
+                  alt=""
+                  className="w-7 h-7 absolute left-11 top-[34px] overflow-visible"
+                  width={28}
+                  height={28}
+                />
+              </div>
+              <div className="bg-[var(--osnovnoy-cvet-2)] rounded-[50px] w-[910px] h-[162px] absolute left-0 top-[272px] overflow-hidden">
+                <div className="flex flex-col gap-3 items-start justify-start max-w-[484px] absolute left-[92px] top-8">
+                  <div className="text-[var(--cvet-teksta-2)] text-left text-2xl font-bold relative w-[484px] flex items-center justify-start">
+                    Разработку продающих веб-сайтов
+                  </div>
+                  <div className="text-[var(--cvet-teksta-2)] text-left text-2xl font-normal relative w-[275px] flex items-center justify-start">
+                    с SEO-оптимизацией
+                  </div>
+                </div>
+                <Image
+                  src="/portal28.svg"
+                  alt=""
+                  className="w-7 h-7 absolute left-11 top-[34px] overflow-visible"
+                  width={28}
+                  height={28}
+                />
+              </div>
+            </div>
+
+            <p className="text-[var(--cvet-teksta)] text-left text-[24px] font-normal text-wrap w-[782px]">
+              Доверьте создание и продвижение сайта нам —<br /> закажите сайт
+              под ключ и получите актив, который будет работать на ваш успех!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
